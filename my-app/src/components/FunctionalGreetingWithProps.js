17e162292226fc143c7d07e2199887eba9aed40e
Ila-bura <>
2023-11-20 17:41:20 +0000

    Create a few props

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -5,6 +5,9 @@ import React from "react";
 // This is a stateless functional component, it doesn't change
 //}
 
-const FunctionalGreetingWithProps = () => <h1>Hello bonkers</h1>;
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello, {props.name}! I see you're {props.age} years old! {props.greeting}</h1>;
+}
 
 export default FunctionalGreetingWithProps;

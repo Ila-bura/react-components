edd84a09b1fa01abb2179d87b7bc2863b5d300b3
Ila-bura <>
2023-11-20 17:30:02 +0000

    Create new component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//function FunctionalGreeting() {
+//    return <h1>Hello from React!</h1>
+// This is a stateless functional component, it doesn't change
+//}
+
+const FunctionalGreeting = () => <h1>Hello from React!!!</h1>;
+
+export default FunctionalGreeting;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//function FunctionalGreeting() {
+//    return <h1>Hello from React!</h1>
+// This is a stateless functional component, it doesn't change
+//}
+
+const FunctionalGreetingWithProps = () => <h1>Hello bonkers</h1>;
+
+export default FunctionalGreetingWithProps;
